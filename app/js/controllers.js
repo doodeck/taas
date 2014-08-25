9ce3350eb7ccf1047a8442a30f1c0613c4362201
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2014-08-25 17:15:12 +0000

    made minimalistic changes to add AngularFire, both single values and arrays

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,37 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('MyCtrl1', ['$scope', '$firebase',
+    function($scope, $firebase) {
+      var bindingTo = true;
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+      var ref = new Firebase("https://mytestchat.firebaseio.com/");
 
-  }]);
+      // create an AngularFire reference to the data
+      var sync = $firebase(ref);
+
+      if (!bindingTo) {
+        // download the data into a local object
+        $scope.data = sync.$asObject();//
+      } else {
+        //
+        var syncObject = sync.$asObject(); // simple key/value data
+
+        // synchronize the object with a three-way data binding
+        // click on `index.html` above to see it used in the DOM!
+        syncObject.$bindTo($scope, "data");
+        //
+      }
+    }])
+  .controller('MyCtrl2', ['$scope', '$firebase',
+    function($scope, $firebase) {
+      var ref = new Firebase("https://mytestchat.firebaseio.com/messages");
+      var sync = $firebase(ref);
+
+      // create a synchronized array for use in our HTML code
+      $scope.messages = sync.$asArray();
+
+      $scope.addMessage = function(text) {
+        $scope.messages.$add({text: text});
+      }
+    }]);
